cb26d29e3bd25bd0d08e548a8207c6345e0a8db0
rishabt08 <rishabht123@gmail.com>
2023-09-22 21:55:34 +0530

    first commit

---
+++ event-bus/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+const axios = require('axios')
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const events = [];
+
+app.post("/events", (req, res) => {
+    const event = req.body;
+    events.push(event);
+
+    axios.post('http://posts-clusterip-srv:4000/events' , event);
+    axios.post('http://comments-clusterip-srv:4001/events' , event);
+    axios.post('http://query-clusterip-srv:4002/events' , event);
+    axios.post('http://moderations-srv:4003/events' , event);
+    
+    res.send({status: 'OK'})
+});
+
+app.get('/events' , (req , res) => {
+    res.send(events);
+}) 
+
+
+
+
+app.listen(4005, () => {
+    console.log("Event Bus running on port: 4005");
+})
